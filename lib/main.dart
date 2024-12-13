05dbfc36669b80c973116cf28a761fecb0c0b8e0
Amjad Elahi <amjadelahi07@gmail.com>
2024-12-13 23:49:01 +0300

    integrate supabase configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:quran/screens/auth_screens/auth_screen.dart';
-import 'package:quran/screens/home_screen/home_screen.dart';
+import 'package:quran/setup/setup.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL'].toString(),
+    anonKey: dotenv.env['SUPABASE_ANON_KEY'].toString(),
+  );
+  await setup();
   runApp(
     DevicePreview(
       enabled: true,
@@ -20,7 +28,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: AuthScreen(),
     );
   }
 }
